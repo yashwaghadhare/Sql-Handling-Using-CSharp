9c559cd124e89e81ef6fd287d4d879093d7b9d89
Yash <127091514+yashwaghadhare@users.noreply.github.com>
2023-06-08 08:38:12 -0700

    SQL-Entity-Framework-PivoteQuery

---
+++ SQL-Entity-Framework-PivoteQuery/SQL-Entity-Framework-PivoteQuery/index.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="SQL_Entity_Framework_PivoteQuery.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="frmIndex" runat="server">
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:GridView ID="dtgGeneratePivoteQeries" runat="server" Width="448px"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SQL-Entity-Framework-PivoteQuery/SQL-Entity-Framework-PivoteQuery/index.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SQL_Entity_Framework_PivoteQuery
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GeneratePivoteQeries();
+        }
+
+        private void GeneratePivoteQeries()
+        {
+            // Assuming you have an instance of your DbContext called "db"
+            using (var db = new DatabaseEntities())
+            {
+                // Pivot query to get the desired data
+                var query = from mark in db.Marks
+                            join student in db.Students on mark.StudentId equals student.Id
+                            group mark by new { student.Name } into pivotGroup
+                            select new
+                            {
+                                StudentName = pivotGroup.Key.Name,
+                                Hindi = pivotGroup.FirstOrDefault(m => m.Subject == "Hindi").Marks,
+                                English = pivotGroup.FirstOrDefault(m => m.Subject == "English").Marks
+                            };
+
+                // Bind the resulting data to the GridView
+                dtgGeneratePivoteQeries.DataSource = query.ToList();
+                dtgGeneratePivoteQeries.DataBind();
+            }
+        }
+    }
+}
